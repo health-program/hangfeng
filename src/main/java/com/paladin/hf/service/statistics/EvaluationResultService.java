b9d074ad037f6efe2a53c95c2650ff87b72f84c0
Tonto <823498927@qq.com>
2019-01-04 14:32:17 +0800

    444

--- src/main/java/com/paladin/hf/service/statistics/EvaluationResultService.java
+++ src/main/java/com/paladin/hf/service/statistics/EvaluationResultService.java
@@ -41,105 +41,102 @@ public class EvaluationResultService {
 		Page<EvaluationResultVO> page = PageHelper.offsetPage(query.getOffset(), query.getLimit());// 分页
 		evaluationResultMapper.userAll(query, DataPermissionUtil.getUnitQueryDouble(query.getUnitId()));
 		return page;
-	};
+	}
 
 	public Page<EvaluationResultVO> evaluationInfo(AppraisalSummaryQueryDTO query) {
         Page<EvaluationResultVO> page = PageHelper.offsetPage(query.getOffset(), query.getLimit());// 分页
         evaluationResultMapper.evaluationInfo(query,DataPermissionUtil.getUnitQueryDouble(query.getUnitId()));
         return page;
-    };
+    }
     
     public Page<EvaluationResultVO> evaluationPeople(AppraisalSummaryQueryDTO query) {
         Page<EvaluationResultVO> page = PageHelper.offsetPage(query.getOffset(), query.getLimit());// 分页
         evaluationResultMapper.evaluationPeople(query);
         return page;
-    };
-    
-    
-    
-    
-    
+    }
 	
 	public void export(OutputStream output, AppraisalSummaryQueryDTO query) {
-		String cycleId = query.getAssessCycleId();
-		String unitId = query.getUnitId();
-		
-		if(cycleId == null || cycleId.length() == 0) {
-			throw new BusinessException("请选择一个考评周期");
-		}
-		
-		if(unitId == null || unitId.length() == 0) {			
-			HfUserSession session = HfUserSession.getCurrentUserSession();
-			if(session.isAdminRoleLevel()) {
-				throw new BusinessException("导出的数据量可能很大，请选择一个机构或科室");
-			}
-		}
-				
-		UnitQuery unitQuery = DataPermissionUtil.getUnitQueryDouble(unitId);
-
-		List<EvaluationResultVO> list = evaluationResultMapper.evaluationInfo(query, unitQuery);
-		if (list == null || list.size() == 0) {
-			throw new BusinessException("没有可导的数据");
-		}
-
-		AssessCycle cycle = assessCycleService.get(cycleId);
-		if (cycle == null) {
-			throw new BusinessException("没有可导的数据");
-		}
-		String cycleName = cycle.getCycleName();
-		for (EvaluationResultVO r : list) {
-			r.setAssessCycleName(cycleName);
-		}
-
-		try {
-			Workbook workbook = new XSSFWorkbook();
-			ExcelWriter<EvaluationResultVO> writer = new DefaultExcelWriter<EvaluationResultVO>(workbook, EvaluationResultVO.class);
-			writer.writeTitle();
-			writer.write(list);
-			writer.output(output);
-		} catch (Exception e) {
-			throw new BusinessException("导出EXCEL文件出错", e);
-		}
-	};
+//		String cycleId = query.getAssessCycleId();
+//		String unitId = query.getUnitId();
+//		
+//		if(cycleId == null || cycleId.length() == 0) {
+//			throw new BusinessException("请选择一个考评周期");
+//		}
+//		
+//		if(unitId == null || unitId.length() == 0) {			
+//			HfUserSession session = HfUserSession.getCurrentUserSession();
+//			if(session.isAdminRoleLevel()) {
+//				throw new BusinessException("导出的数据量可能很大，请选择一个机构或科室");
+//			}
+//		}
+//				
+//		UnitQuery unitQuery = DataPermissionUtil.getUnitQueryDouble(unitId);
+//
+//		List<EvaluationResultVO> list = evaluationResultMapper.evaluationInfo(query, unitQuery);
+//		if (list == null || list.size() == 0) {
+//			throw new BusinessException("没有可导的数据");
+//		}
+//
+//		AssessCycle cycle = assessCycleService.get(cycleId);
+//		if (cycle == null) {
+//			throw new BusinessException("没有可导的数据");
+//		}
+//		String cycleName = cycle.getCycleName();
+//		for (EvaluationResultVO r : list) {
+//			r.setAssessCycleName(cycleName);
+//		}
+//
+//		try {
+//			Workbook workbook = new XSSFWorkbook();
+//			ExcelWriter<EvaluationResultVO> writer = new DefaultExcelWriter<EvaluationResultVO>(workbook, EvaluationResultVO.class);
+//			writer.writeTitle();
+//			writer.write(list);
+//			writer.output(output);
+//		} catch (Exception e) {
+//			throw new BusinessException("导出EXCEL文件出错", e);
+//		}
+		//TODO 黄伟华
+	}
 	
    public void exportPeople(OutputStream output, AppraisalSummaryQueryDTO query) {
-        String cycleId = query.getAssessCycleId();
-        String unitId = query.getUnitId();
-        
-        if(cycleId == null || cycleId.length() == 0) {
-            throw new BusinessException("请选择一个考评周期");
-        }
-        
-        if(unitId == null || unitId.length() == 0) {            
-            HfUserSession session = HfUserSession.getCurrentUserSession();
-            if(session.isAdminRoleLevel()) {
-                throw new BusinessException("导出的数据量可能很大，请选择一个机构或科室");
-            }
-        }
-                
-        List<EvaluationResultExcelDTO> list = evaluationResultMapper.excelPeople(query);
-        if (list == null || list.size() == 0) {
-            throw new BusinessException("没有可导的数据");
-        }
-
-        AssessCycle cycle = assessCycleService.get(cycleId);
-        if (cycle == null) {
-            throw new BusinessException("没有可导的数据");
-        }
-        String cycleName = cycle.getCycleName();
-        for (EvaluationResultExcelDTO r : list) {
-            r.setAssessCycleName(cycleName);
-        }
-
-        try {
-            Workbook workbook = new XSSFWorkbook();
-            ExcelWriter<EvaluationResultExcelDTO> writer = new DefaultExcelWriter<EvaluationResultExcelDTO>(workbook, EvaluationResultExcelDTO.class);
-            writer.writeTitle();
-            writer.write(list);
-            writer.output(output);
-        } catch (Exception e) {
-            throw new BusinessException("导出EXCEL文件出错", e);
-        }
-    };
+//        String cycleId = query.getAssessCycleId();
+//        String unitId = query.getUnitId();
+//        
+//        if(cycleId == null || cycleId.length() == 0) {
+//            throw new BusinessException("请选择一个考评周期");
+//        }
+//        
+//        if(unitId == null || unitId.length() == 0) {            
+//            HfUserSession session = HfUserSession.getCurrentUserSession();
+//            if(session.isAdminRoleLevel()) {
+//                throw new BusinessException("导出的数据量可能很大，请选择一个机构或科室");
+//            }
+//        }
+//                
+//        List<EvaluationResultExcelDTO> list = evaluationResultMapper.excelPeople(query);
+//        if (list == null || list.size() == 0) {
+//            throw new BusinessException("没有可导的数据");
+//        }
+//
+//        AssessCycle cycle = assessCycleService.get(cycleId);
+//        if (cycle == null) {
+//            throw new BusinessException("没有可导的数据");
+//        }
+//        String cycleName = cycle.getCycleName();
+//        for (EvaluationResultExcelDTO r : list) {
+//            r.setAssessCycleName(cycleName);
+//        }
+//
+//        try {
+//            Workbook workbook = new XSSFWorkbook();
+//            ExcelWriter<EvaluationResultExcelDTO> writer = new DefaultExcelWriter<EvaluationResultExcelDTO>(workbook, EvaluationResultExcelDTO.class);
+//            writer.writeTitle();
+//            writer.write(list);
+//            writer.output(output);
+//        } catch (Exception e) {
+//            throw new BusinessException("导出EXCEL文件出错", e);
+//        }
+	   //TODO 黄伟华
+    }
 
 }
--- src/main/java/com/paladin/hf/service/statistics/vo/EvaluationResultVO.java
+++ src/main/java/com/paladin/hf/service/statistics/vo/EvaluationResultVO.java
@@ -1,10 +1,6 @@
 package com.paladin.hf.service.statistics.vo;
 
-import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.paladin.framework.common.OffsetPage;
-import com.paladin.framework.core.container.ConstantsContainer;
 import com.paladin.framework.core.exception.BusinessException;
 import com.paladin.framework.excel.write.WriteProperty;
 import com.paladin.hf.core.UnitContainer;
@@ -15,24 +11,10 @@ import com.paladin.hf.core.UnitContainer;
  * @version 2018年3月7日 下午2:52:54
  */
 public class EvaluationResultVO extends OffsetPage {
-	private String Id;
-	//@WriteProperty(cellIndex = 2, name = "姓名", width = 20)
-	private String name;
-	private String selfAssGrade;
-	private Date selfAssTime;
-	private String departAssGrade;
-	private Date departAssTime;
-	private String unitAssGrade;
-	private Date unitAssTime;
-	private String confirmedResult;
-	private Date confirmedTime;
-	private String operateState;
+	
+	private String id;
 	private String unitId;
 	private String agencyId;
-	private String assessCycleId;
-	private String unitAssGradeCOUNT;
-	private String notUnitAssGradeCOUNT;
-	private String total;
 
 	// 以下用于导出EXCEL
 	@WriteProperty(cellIndex = 0, name = "所属机构", width = 30)	
@@ -47,58 +29,7 @@ public class EvaluationResultVO extends OffsetPage {
     private String notUnitAssGradeExcelCOUNT;
 	@WriteProperty(cellIndex = 5, name = "总人数", width = 20)
     private String totalExcel;
-	/*@WriteProperty(cellIndex = 4, name = "自评等级", width = 10)
-	private String selfAssGradeName;
-	@WriteProperty(cellIndex = 5, name = "科室考评等级", width = 15)
-	private String departAssGradeName;
-	@WriteProperty(cellIndex = 6, name = "机构考评等级", width = 15)
-	private String unitAssGradeName;
-	@WriteProperty(cellIndex = 7, name = "被考评人确认结果", width = 20)
-	private String confirmedResultName;
-	@WriteProperty(cellIndex = 8, name = "考评状态", width = 20)
-	private String operateStateName;*/
 	
-
-	public String getUnitId() {
-		return unitId;
-	}
-
-	public void setUnitId(String unitId) {
-		this.unitId = unitId;
-	}
-
-	public String getId() {
-		return Id;
-	}
-
-	public void setId(String id) {
-		Id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUnitAssGrade() {
-		return unitAssGrade;
-	}
-
-	public void setUnitAssGrade(String unitAssGrade) {
-		this.unitAssGrade = unitAssGrade;
-	}
-
-	public String getOperateState() {
-		return operateState;
-	}
-
-	public void setOperateState(String operateState) {
-		this.operateState = operateState;
-	}
-
 	public String getUnitName() {
 		try {
 			return UnitContainer.getUnitName(unitId);
@@ -115,60 +46,20 @@ public class EvaluationResultVO extends OffsetPage {
 		}
 	}
 
-	public String getSelfAssGrade() {
-		return selfAssGrade;
+	public String getId() {
+		return id;
 	}
 
-	public void setSelfAssGrade(String selfAssGrade) {
-		this.selfAssGrade = selfAssGrade;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public Date getSelfAssTime() {
-		return selfAssTime;
+	public String getUnitId() {
+		return unitId;
 	}
 
-	public void setSelfAssTime(Date selfAssTime) {
-		this.selfAssTime = selfAssTime;
-	}
-
-	public String getDepartAssGrade() {
-		return departAssGrade;
-	}
-
-	public void setDepartAssGrade(String departAssGrade) {
-		this.departAssGrade = departAssGrade;
-	}
-
-	public Date getDepartAssTime() {
-		return departAssTime;
-	}
-
-	public void setDepartAssTime(Date departAssTime) {
-		this.departAssTime = departAssTime;
-	}
-
-	public Date getUnitAssTime() {
-		return unitAssTime;
-	}
-
-	public void setUnitAssTime(Date unitAssTime) {
-		this.unitAssTime = unitAssTime;
-	}
-
-	public String getConfirmedResult() {
-		return confirmedResult;
-	}
-
-	public void setConfirmedResult(String confirmedResult) {
-		this.confirmedResult = confirmedResult;
-	}
-
-	public Date getConfirmedTime() {
-		return confirmedTime;
-	}
-
-	public void setConfirmedTime(Date confirmedTime) {
-		this.confirmedTime = confirmedTime;
+	public void setUnitId(String unitId) {
+		this.unitId = unitId;
 	}
 
 	public String getAgencyId() {
@@ -179,87 +70,6 @@ public class EvaluationResultVO extends OffsetPage {
 		this.agencyId = agencyId;
 	}
 
-	public String getAssessCycleId() {
-		return assessCycleId;
-	}
-
-	public void setAssessCycleId(String assessCycleId) {
-		this.assessCycleId = assessCycleId;
-	}
-
-	public String getUnitAssGradeCOUNT()
-    {
-        return unitAssGradeCOUNT;
-    }
-
-
-    public String getNotUnitAssGradeCOUNT()
-    {
-        return notUnitAssGradeCOUNT;
-    }
-
-    public void setNotUnitAssGradeCOUNT(String notUnitAssGradeCOUNT)
-    {
-        this.notUnitAssGradeCOUNT = notUnitAssGradeCOUNT;
-    }
-
-    public String getTotal()
-    {
-        return total;
-    }
-
-    public void setTotal(String total)
-    {
-        this.total = total;
-    }
-    
-    
-    @JsonIgnore
-    public String getUnitAssGradeExcelCOUNT()
-    {
-        return unitAssGradeCOUNT;
-    }
-
-    @JsonIgnore
-    public String getNotUnitAssGradeExcelCOUNT()
-    {
-        return notUnitAssGradeCOUNT;
-    }
-
-    @JsonIgnore
-    public String getTotalExcel()
-    {
-        return total;
-    }
-
-
-    @JsonIgnore
-	public String getSelfAssGradeName() {
-		return selfAssGrade == null || selfAssGrade.length() == 0 ? "" : ConstantsContainer.getValue("ass-grades", selfAssGrade);
-	}
-
-	@JsonIgnore
-	public String getDepartAssGradeName() {
-		return departAssGrade == null || departAssGrade.length() == 0 ? "" : ConstantsContainer.getValue("ass-grades", departAssGrade);
-	}
-
-	@JsonIgnore
-	public String getUnitAssGradeName() {
-		return unitAssGrade == null || unitAssGrade.length() == 0 ? "" : ConstantsContainer.getValue("ass-grades", unitAssGrade);
-
-	}
-
-	@JsonIgnore
-	public String getConfirmedResultName() {
-		return confirmedResult == null || confirmedResult.length() == 0 ? "未确认" : (confirmedResult.equals("1") ? "同意" : "不同意");
-	}
-	
-	@JsonIgnore
-	public String getOperateStateName() {
-		return operateState == null || operateState.length() == 0 ? "未考评" : ConstantsContainer.getValue("operateState", operateState);
-	}
-	
-	@JsonIgnore
 	public String getAssessCycleName() {
 		return assessCycleName;
 	}
@@ -267,5 +77,30 @@ public class EvaluationResultVO extends OffsetPage {
 	public void setAssessCycleName(String assessCycleName) {
 		this.assessCycleName = assessCycleName;
 	}
+
+	public String getUnitAssGradeExcelCOUNT() {
+		return unitAssGradeExcelCOUNT;
+	}
+
+	public void setUnitAssGradeExcelCOUNT(String unitAssGradeExcelCOUNT) {
+		this.unitAssGradeExcelCOUNT = unitAssGradeExcelCOUNT;
+	}
+
+	public String getNotUnitAssGradeExcelCOUNT() {
+		return notUnitAssGradeExcelCOUNT;
+	}
+
+	public void setNotUnitAssGradeExcelCOUNT(String notUnitAssGradeExcelCOUNT) {
+		this.notUnitAssGradeExcelCOUNT = notUnitAssGradeExcelCOUNT;
+	}
+
+	public String getTotalExcel() {
+		return totalExcel;
+	}
+
+	public void setTotalExcel(String totalExcel) {
+		this.totalExcel = totalExcel;
+	}
+	
 
 }

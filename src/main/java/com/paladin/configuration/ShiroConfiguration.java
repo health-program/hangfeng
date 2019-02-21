325b42163a1c5f3ce2ea8ac847326f5c6cbe1634
Mykites <32703366+Mykites@users.noreply.github.com>
2019-02-21 10:35:45 +0800

    app

--- src/main/java/com/paladin/configuration/ShiroConfiguration.java
+++ src/main/java/com/paladin/configuration/ShiroConfiguration.java
@@ -284,6 +284,7 @@ public class ShiroConfiguration {
 		// <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
 		filterChainDefinitionMap.put("/static/**", "anon");
 		filterChainDefinitionMap.put("/file/**", "anon");
+		filterChainDefinitionMap.put("/app/**", "anon");
 		// filterChainDefinitionMap.put("/favicon.ico", "anon");
 
 		filterChainDefinitionMap.put("/hf/logout", "logout");
